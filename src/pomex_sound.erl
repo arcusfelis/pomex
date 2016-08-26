2c631f0d92aea84df949ad439745bf169c822d1b
Michael Uvarov <arcusfelis@gmail.com>
2016-08-26 11:39:14 +0200

    Add module to play sounds

---
+++ src/pomex_sound.erl
@@ -0,0 +1,27 @@
+-module(pomex_sound).
+-export([play_start/0,
+         play_finish/0]).
+
+play_start() ->
+    play_file(find_file("start.wav")).
+
+play_finish() ->
+    play_file(find_file("finish.wav")).
+
+find_file(Name) ->
+    filename:absname(Name, priv_dir()).
+
+priv_dir() ->
+    case code:priv_dir(pomex) of
+        [_|_] = PrivDir ->
+            PrivDir;  
+        _ ->
+            "priv"
+    end.
+
+play_file(File) ->
+    stop_playing(),
+    erlang:open_port({spawn_executable, "/usr/bin/env"}, [{args, ["aplay", File]}]).
+
+stop_playing() ->
+    os:cmd("killall aplay").
